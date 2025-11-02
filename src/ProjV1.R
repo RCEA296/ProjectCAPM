ac8815166f5c19d300c81177bc30a51966bb12e1
100553521-debug <100553521@alumnos.uc3m.es>
2025-11-02 21:07:41 +0100

    A Rayane le gusta la pollita

--- src/ProjV1.R
+++ src/ProjV1.R
@@ -37,12 +37,48 @@ tsunami_data %>% select(sig, nst, dmin, gap, depth) %>% stack()%>%
   labs(title = "Univariate Boxplots per Variable", x = "", y = "Value") +
   theme_bw()
 
-#From this plot we can infer that most
+#From this plot we can infer that most values are concentrated at 0. But it might
+#have a huge impact in tsunami prediction due to the nature of variables.
+tsunami_data %>% count(tsunami) #We can see 2/3 of the times there was no tsunami, 
+#so these parameters would indicate the absence of factors that determine a tsunami
 
-tsunami_data %>% 
+#Here, oultiers can actually contribute to a models prediction, therefore only
+#extreme values are deleted. Thinking instead about transforming data instead of 
+# deleting it is the correct approach
+
+tsunami_data %>% select(sig, nst, dmin, gap, depth) %>% stack()%>% 
+  ggplot(aes(x = values)) +
+  geom_histogram(bins = 30, fill = "lightblue", color = "black") +
+  facet_wrap(~ ind, scales = "free") +  # separate y-axis for each variable
+  labs(title = "Distribution of Variables", x = "Value", y = "Count") +
+  theme_bw()
+
+#After seeing the distributions we can determine that a logarithmic transformation of this variables
+#is the best way to proceed. This observation completely matches with how magnitude is measured
+
+tsunami_data = tsunami_data %>%
+  mutate(across(c(sig, nst, gap, dmin, depth),
+                ~ log10(.x + 1)))
+
+#Now we can see the the transformation
+tsunami_data %>% select(sig, nst, dmin, gap, depth) %>% stack()%>% 
+  ggplot(aes(x = values)) +
+  geom_histogram(bins = 30, fill = "lightblue", color = "black") +
+  facet_wrap(~ ind, scales = "free") +  # separate y-axis for each variable
+  labs(title = "Distribution of Variables", x = "Value", y = "Count") +
+  theme_bw()
+
+normalized_tsunami_data = tsunami_data %>%
+  mutate(across(-tsunami,~ scale(.x)))
+  
+
 
 #Cite
 #https://creativecommons.org/licenses/by/4.0/
+
+
+
+
 
 
 
