238cc530808019c2e493e17a03c5afd03ca03f79
100553521-debug <100553521@alumnos.uc3m.es>
2025-11-02 19:19:19 +0100

    p

--- src/ProjV1.R
+++ src/ProjV1.R
@@ -1,34 +1,48 @@
----
-title: "Tsunami-Prediction"
-author: "Angel Ortiz; Rayane Cascon"
-date: 'UC3M, 2025'
-output:
-  html_document:
-    css: my-theme.css
-    theme: cerulean
-    highlight: tango
-    number_sections: no
-    toc: no
-    toc_depth: 1
-editor_options:
-  chunk_output_type: console
----
 
-```{r global_options, include =TRUE, echo =FALSE}
-knitr::opts_chunk$set(echo = TRUE, warning = FALSE, message = FALSE)
-```
-
-
-Libraries
-```{r}
 library("dplyr")
-```
-
-```{r}
+library("ggplot2")
+rm(list = ls())
 tsunami_data = read.csv("src/data/TsunamiData.csv", header = TRUE)
-```
 
+#The aim of study of this data set is to achieve to predict a Tsunami event and 
+#look for the most relevant indicators 
 
+#Feature Engineering
+
+##Remove variables without useful information. 
+#Categorical variables will be treated first
+tsunami_data %>% count(alert)
+
+tsunami_data = tsunami_data %>% 
+  select(-c("title", "continent", "location", "country", "net","alert"))
+
+#Different methods were used to measure earthquakes magnitude. Therefore, 
+#a bias arises. To solve this, we filter by the moment magnitude based 
+#methods, which are the modern standard and more accurate measurements (indicated with a "w" suffix)
+
+tsunami_data %>% count(magType)#There is no significant loss of observations
+
+tsunami_data = tsunami_data%>% 
+  filter(magType %in% c("mw","mwb","mwc","mww"))  %>% select(-magType)
+
+tsunami_data = tsunami_data %>% mutate(year = as.numeric(substr(date_time, 7,11)))%>% 
+  select(-date_time)# We will focus on tendencies over the years, so we can convert the variable "year" into a numeric value
+
+#We now have only numerical values. We must look for outliers
+tsunami_data %>% select(sig, nst, dmin, gap, depth) %>% stack()%>% 
+  
+  ggplot(aes(y = values)) +
+  geom_boxplot(outlier.color = "red", fill = "lightblue") +
+  facet_wrap(~ ind, scales = "free") + 
+  labs(title = "Univariate Boxplots per Variable", x = "", y = "Value") +
+  theme_bw()
+
+#From this plot we can infer that most
+
+tsunami_data %>% 
+
+#Cite
+#https://creativecommons.org/licenses/by/4.0/
 
 
 
